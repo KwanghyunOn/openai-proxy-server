350b4a65e8dc8bd10577630711f988c410725528
Kwanghyun On <kwanghyun.on@gmail.com>
2024-09-24 11:23:18 +0900

    Test proxy server

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
-const app = require("./server")
+// const app = require("./server")
+const app = require("./proxy-server")
 
 module.exports = (req, res) => {
   app(req, res)
---
+++ api/proxy-server.js
@@ -0,0 +1,35 @@
+const express = require("express")
+const { createProxyMiddleware } = require("http-proxy-middleware")
+const morgan = require("morgan")
+
+const app = express()
+const PORT = 3000
+const TARGET = "https://api.openai.com"
+
+// Logging middleware
+app.use(morgan("dev"))
+
+// Create the proxy middleware
+const proxy = createProxyMiddleware({
+  target: TARGET,
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    console.log(`Proxying: ${req.method} ${req.url} -> ${TARGET}${req.url}`)
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    console.log(
+      `Received response: ${proxyRes.statusCode} for ${req.method} ${req.url}`
+    )
+  },
+  onError: (err, req, res) => {
+    console.error("Proxy Error:", err)
+    res.status(500).send("Proxy Error")
+  },
+})
+
+app.use("/", proxy)
+
+app.listen(PORT, () => {
+  console.log(`Transparent proxy server is running on http://localhost:${PORT}`)
+  console.log(`Proxying all requests to ${TARGET}`)
+})
