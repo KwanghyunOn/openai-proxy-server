d19d793364df05ff548c3762a6dd05af26d7a1be
Kwanghyun On <kwanghyun.on@gmail.com>
2024-09-24 12:22:48 +0900

    Handle binary response

--- api/proxy-server.js
+++ api/proxy-server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { createProxyMiddleware } = require("http-proxy-middleware")
 const morgan = require("morgan")
+const zlib = require("zlib")
 
 const app = express()
 const PORT = 3000
@@ -24,9 +25,37 @@ app.use((req, res, next) => {
 
   res.end = function (chunk) {
     if (chunk) chunks.push(chunk)
-    const body = Buffer.concat(chunks).toString("utf8")
+
+    const contentEncoding = res.getHeader("content-encoding")
+    const contentType = res.getHeader("content-type")
+
     console.log(`Response: ${res.statusCode}`)
-    console.log(`Response body: ${body}`)
+    console.log(`Content-Type: ${contentType}`)
+    console.log(`Content-Encoding: ${contentEncoding}`)
+
+    if (contentType?.includes("text/event-stream")) {
+      console.log("Streaming response detected - body not logged")
+    } else {
+      const buffer = Buffer.concat(chunks)
+
+      let body
+      try {
+        if (contentEncoding === "gzip") {
+          body = zlib.gunzipSync(buffer).toString("utf8")
+        } else if (contentEncoding === "deflate") {
+          body = zlib.inflateSync(buffer).toString("utf8")
+        } else if (contentEncoding === "br") {
+          body = zlib.brotliDecompressSync(buffer).toString("utf8")
+        } else {
+          body = buffer.toString("utf8")
+        }
+        console.log(`Response body: ${body}`)
+      } catch (error) {
+        console.error(`Error decoding response body: ${error.message}`)
+        console.log(`Raw response body (hex): ${buffer.toString("hex")}`)
+      }
+    }
+
     oldEnd.apply(res, arguments)
   }
 
